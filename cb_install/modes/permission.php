ac466c603fa4e52012c07974167aa50a8f2133ad
amouhzi <github@anezi.net>
2013-10-02 21:20:43 +0200

    End of file

--- cb_install/modes/permission.php
+++ cb_install/modes/permission.php
@@ -1,37 +1,45 @@
 
 <h2>Checking File &amp; Directories Permissions</h2>
-    ClipBucket need some files and folders permissions in order to store files properly, please make sure all files given below are chmod properly<br />
-<em>CHMOD : the chmod command (abbreviated from <strong>ch</strong>ange <strong>mod</strong>e) is a shell command and C language function in Unix and Unix-like  environments.</em></p>
-	
-</p>
-
+ClipBucket need some files and folders permissions in order to store
+files properly, please make sure all files given below are chmod
+properly
+<br />
+<em>CHMOD : the chmod command (abbreviated from <strong>ch</strong>ange
+	<strong>mod</strong>e) is a shell command and C language function in
+	Unix and Unix-like environments.
+</em>
 
 <dl>
-<?php $permissions = checkPermissions(); ?>
-<?php
-    foreach($permissions as $permission)
-    {
-        ?>
-        <dt style="width:300px"><?php echo $permission['path']?></dt>
-        
-        <dd><?php echo msg_arr($permission);?></dd>
-        <?
-    }
-?>
-    
+	<?php $permissions = checkPermissions(); ?>
+	<?php
+	foreach($permissions as $permission)
+	{
+		?>
+	<dt style="width: 300px">
+		<?php echo $permission['path']?>
+	</dt>
+
+	<dd>
+		<?php echo msg_arr($permission);?>
+	</dd>
+	<?
+	}
+	?>
+
 </dl>
 
 
 <form name="installation" method="post" id="installation">
-    <div style="padding:10px 0px" align="right">
-	<?php echo button('Recheck',' onclick="$(\'#mode\').val(\'permission\');
+	<div style="padding: 10px 0px" align="right">
+		<?php echo button('Recheck',' onclick="$(\'#mode\').val(\'permission\');
 	 $(\'#installation\').submit()" ',true);?>
-     
-	<?php echo button('Continue To Next Step',' onclick="$(\'#installation\').submit()" ');?></div>
-    
-    <?php if(!$upgrade): ?>
-    <input type="hidden" name="mode" value="database"  id="mode"/>
-    <?php else: ?>
-    <input type="hidden" name="mode" value="dataimport"  id="mode"/>
-    <?php endif; ?>
+
+		<?php echo button('Continue To Next Step',' onclick="$(\'#installation\').submit()" ');?>
+	</div>
+
+	<?php if(!$upgrade): ?>
+	<input type="hidden" name="mode" value="database" id="mode" />
+	<?php else: ?>
+	<input type="hidden" name="mode" value="dataimport" id="mode" />
+	<?php endif; ?>
 </form>
