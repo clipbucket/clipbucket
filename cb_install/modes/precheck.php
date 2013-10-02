6fb1ed2edc68858ff4cce2cc4355a2e1a14c8570
amouhzi <github@anezi.net>
2013-10-02 20:51:38 +0200

    Replace <? with <?php

--- cb_install/modes/precheck.php
+++ cb_install/modes/precheck.php
@@ -7,23 +7,23 @@ Clipbucket requires following modules in order to work properly, we are performi
 
 <dl>
     <dt>PHP</dt>
-    	<? $php_ver = check_module('php'); ?>
+    	<?php $php_ver = check_module('php'); ?>
     	<dd><?=msg_arr($php_ver);?></dd>
     <dt>FFMPEG</dt>
-    	<? $ffmpeg_ver = check_module('ffmpeg'); ?>
+    	<?php $ffmpeg_ver = check_module('ffmpeg'); ?>
     <dd><?=msg_arr($ffmpeg_ver);?></dd>
     <dt>FLVtool2</dt>
-         <? $flvtool2_ver = check_module('flvtool2'); ?>
+         <?php $flvtool2_ver = check_module('flvtool2'); ?>
     	<dd><?=msg_arr($flvtool2_ver);?></dd>
         
     <dt>MP4Box</dt>
-    	<? $mp4boxver = check_module('mp4box'); ?>
+    	<?php $mp4boxver = check_module('mp4box'); ?>
     	<dd><?=msg_arr($mp4boxver);?></dd>
     <dt>cURL</dt>
-    	<? $curlver = check_module('curl'); ?>
+    	<?php $curlver = check_module('curl'); ?>
     	<dd><?=msg_arr($curlver);?></dd>
     <dt>PHPShield</dt>
-    	<? $phpshield = check_module('phpshield'); ?>
+    	<?php $phpshield = check_module('phpshield'); ?>
     	<dd><?=msg_arr($phpshield);?></dd>
 </dl>
 <p></p>
