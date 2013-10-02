e6ad00d52f736df8d5c1f48337600a83638269ac
amouhzi <github@anezi.net>
2013-10-02 22:09:22 +0200

    remove  -s {$width}x{$height}

--- includes/classes/conversion/ffmpeg.class.php
+++ includes/classes/conversion/ffmpeg.class.php
@@ -179,7 +179,7 @@ class ffmpeg
 		$use_vf = config('use_ffmpeg_vf');
 		if($use_vf=='no')
 		{
-		$opt_av .= " -s {$width}x{$height} ";
+		$opt_av .= " ";
 		}else
 		{
 			$opt_av .= "-s {$width}x{$height} -aspect  $ratio -vf  pad=0:0:0:0:black";
