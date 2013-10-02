09795d6d6460b250dbec640f972bebcefa14a080
amouhzi <github@anezi.net>
2013-10-02 21:57:19 +0200

    remove -padtop $pad_top

--- includes/classes/conversion/ffmpeg.class.php
+++ includes/classes/conversion/ffmpeg.class.php
@@ -179,7 +179,7 @@ class ffmpeg
 		$use_vf = config('use_ffmpeg_vf');
 		if($use_vf=='no')
 		{
-		$opt_av .= " -s {$width}x{$height} -aspect $ratio -padtop $pad_top -padbottom $pad_bottom -padleft $pad_left -padright $pad_right ";
+		$opt_av .= " -s {$width}x{$height} -aspect $ratio -padbottom $pad_bottom -padleft $pad_left -padright $pad_right ";
 		}else
 		{
 			$opt_av .= "-s {$width}x{$height} -aspect  $ratio -vf  pad=0:0:0:0:black";
